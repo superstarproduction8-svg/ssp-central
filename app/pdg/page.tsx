59c10acfea157a4e2c936e5c2a61765699baedfb
superstarproduction8-svg <superstarproduction8@gmail.com>
2026-09-21 13:59:11 -0400

    Delete app/pdg/page.tsx

--- app/pdg/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function PDG() {
-  return (
-    <div style={{ padding: '40px', textAlign: 'center' }}>
-      <h1 style={{ color: '#facc15' }}>Espas PDG</h1>
-      <p style={{ marginTop: '20px' }}>Byenveni PDG - Dashboard ou a ap vini la...</p>
-      <a href="/" style={{ color: 'white', display: 'block', marginTop: '20px' }}>← Retounen Akèy</a>
-    </div>
-  );
-}
