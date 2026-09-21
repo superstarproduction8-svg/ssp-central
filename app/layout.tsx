b39f77fcd2cc47d560a29db4fe561d088fe37f37
superstarproduction8-svg <superstarproduction8@gmail.com>
2026-09-21 14:23:32 -0400

    Delete app directory

--- app/layout.tsx
+++
@@ -1,9 +0,0 @@
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="fr">
-      <body style={{ margin: 0, fontFamily: 'Arial', background: '#0a0a0a', color: 'white' }}>
-        {children}
-      </body>
-    </html>
-  );
-}
